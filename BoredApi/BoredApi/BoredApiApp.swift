f5fec4cb47da271392b91eafa61e216d2b14de12
Huy Lam <>
2022-03-19 21:16:27 +0700

    init project

---
+++ BoredApi/BoredApi/BoredApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoredApiApp.swift
+//  BoredApi
+//
+//  Created by Huy Lam on 19/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct BoredApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoredApi/BoredApi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BoredApi
+//
+//  Created by Huy Lam on 19/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
